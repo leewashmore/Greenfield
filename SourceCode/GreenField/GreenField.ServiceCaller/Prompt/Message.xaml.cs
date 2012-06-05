2c7c3884cf221dc2c680fa945f5ae425d909ef9b
David.Muench <none@none>
2012-06-04 23:58:11 -0400

    Fixed assembly references.

--- SourceCode/GreenField/GreenField.ServiceCaller/Prompt/Message.xaml.cs
+++ SourceCode/GreenField/GreenField.ServiceCaller/Prompt/Message.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Telerik.Windows.Controls;
 
 namespace GreenField.ServiceCaller
 {
