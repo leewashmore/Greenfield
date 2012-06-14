14b652a84236a391676de61239cd4ba845ba731c
seema <none@none>
2012-06-14 14:37:43 +0530

    FX Commodity Changes

--- SourceCode/GreenField/GreenField.Web/Services/ModelFXOperations.svc.cs
+++ SourceCode/GreenField/GreenField.Web/Services/ModelFXOperations.svc.cs
@@ -233,7 +233,7 @@ namespace GreenField.Web.Services
         /// <returns>Commodity Result</returns>
         [OperationContract]
         [FaultContract(typeof(ServiceFault))]
-        //TODO Seema: Add Input Param Country
+        
         public List<FXCommodityData> RetrieveCommodityData(string selectedCommodityID)
         {
             try
@@ -244,54 +244,103 @@ namespace GreenField.Web.Services
                 //if (!isServiceUp)
                 //    throw new Exception();
 
-                List<CommodityResult> resultDB = new List<CommodityResult>();
-                List<FXCommodityData> resultVW = new List<FXCommodityData>();
+                List<CommodityResult> resultDB = new List<CommodityResult>();                
+                List<FXCommodityData> calculatedViewResult = new List<FXCommodityData>();
                 List<FXCommodityData> result = new List<FXCommodityData>();
                 ResearchEntities research = new ResearchEntities();
 
                 DimensionEntitiesService.Entities entity = DimensionEntity;
                 List<DimensionEntitiesService.GF_PRICING_BASEVIEW> dimSvcPricingViewData = null;
+                List<DimensionEntitiesService.GF_SELECTION_BASEVIEW> dimSvcSelectionViewData = null;
+
+                if (entity.GF_SELECTION_BASEVIEW == null && entity.GF_SELECTION_BASEVIEW.Count() == 0)
+                    return null;
 
                 if (entity.GF_PRICING_BASEVIEW == null && entity.GF_PRICING_BASEVIEW.Count() == 0)
                     return null;
-
-                //TODO Seema: Input Parameter has to be added - Country
-                //Retrieving data from database
-                //TODO Seema: Remove HardCoded Value below
-                //selectedCommodityID = "ZINC";
-                resultDB = research.ExecuteStoreQuery<CommodityResult>("exec GetCOMMODITY_FORECASTS @commodityID={0}", selectedCommodityID).ToList();
-
                 
-                //TODO Seema: Input Parameter has to be added - Country
-                //Retrieving Data from WCF svc
-                //dimSvcPricingViewData = entity.GF_PRICING_BASEVIEW.ToList();
-                var res = from p in entity.GF_PRICING_BASEVIEW
-                          where p.INSTRUMENT_ID == Convert.ToString(selectedCommodityID).ToUpper()
-                          select p;
-                dimSvcPricingViewData = res.ToList();
-                foreach(GF_PRICING_BASEVIEW item in dimSvcPricingViewData)
+                //Retrieving data from database             
+                resultDB = research.ExecuteStoreQuery<CommodityResult>("exec GetCOMMODITY_FORECASTS @commodityID={0}", selectedCommodityID).ToList();
+                //resultDB = research.ExecuteStoreQuery<CommodityResult>("exec GetCOMMODITY_FORECASTS").ToList();
+
+                //Retrieving Data from Views
+
+                if (selectedCommodityID != null && selectedCommodityID != string.Empty)
                 {
-                    FXCommodityData data = new FXCommodityData();
-                    data.CommodityID = item.INSTRUMENT_ID;
-                    data.FromDate = item.FROMDATE;
-                    resultVW.Add(data);
+                    if (selectedCommodityID.ToUpper() != GreenfieldConstants.COMMODITY_ALL)
+                        dimSvcSelectionViewData = entity.GF_SELECTION_BASEVIEW
+                                                .Where(g => (g.AIMS_COMMODITY_ID != null) && (g.AIMS_COMMODITY_ID.ToUpper() == selectedCommodityID.ToUpper()))
+                                                .ToList();                
+                    else                
+                        dimSvcSelectionViewData = entity.GF_SELECTION_BASEVIEW
+                                              .Where(g => g.AIMS_COMMODITY_ID != null)
+                                              .ToList();
                 }
-                FXCommodityData calculatedData = FXCommodityCalculations.CalculateCommodityData(resultVW);
+
+                if (dimSvcSelectionViewData != null && dimSvcSelectionViewData.Count > 0)
+                {
+                    foreach (DimensionEntitiesService.GF_SELECTION_BASEVIEW item in dimSvcSelectionViewData)
+                    {
+                        if (item.INSTRUMENT_ID != null && item.INSTRUMENT_ID != string.Empty)
+                        {
+                            dimSvcPricingViewData = entity.GF_PRICING_BASEVIEW
+                                                    .Where(g => g.INSTRUMENT_ID.ToUpper() == item.INSTRUMENT_ID.ToUpper())
+                                                    .ToList();
+                            if (dimSvcPricingViewData != null && dimSvcPricingViewData.Count > 0)
+                            {
+                                List<FXCommodityData> resultView = new List<FXCommodityData>();
+                                foreach (DimensionEntitiesService.GF_PRICING_BASEVIEW itemPricing in dimSvcPricingViewData)
+                                {
+                                    FXCommodityData data = new FXCommodityData();
+
+                                    data.CommodityId = item.AIMS_COMMODITY_ID;    // Assigning Commodity ID from above list
+                                    data.FromDate = itemPricing.FROMDATE;
+                                    data.DailyClosingPrice = itemPricing.DAILY_CLOSING_PRICE;
+                                    data.InstrumentId = item.INSTRUMENT_ID;         //Assigning Instrument Id from above list
+
+                                    resultView.Add(data);
+                                }
+                                FXCommodityData calculatedData = new FXCommodityData();
+
+                                //calling method to perform calculations
+                                FXCommodityData calculatedDataForCommodity = FXCommodityCalculations.CalculateCommodityData(resultView);
+                                calculatedData.CommodityId = calculatedDataForCommodity.CommodityId;
+                                calculatedData.InstrumentId = calculatedDataForCommodity.InstrumentId;
+                                calculatedData.YTD = calculatedDataForCommodity.YTD;
+                                calculatedData.Year1 = calculatedDataForCommodity.Year1;
+                                calculatedData.Year3 = calculatedDataForCommodity.Year3;
+
+                                calculatedViewResult.Add(calculatedData);
+                            }
+
+                        }
+                    }
+                }                
                 for (int _index = 0; _index < resultDB.Count; _index++)
                 {
-                    FXCommodityData commodityData = new FXCommodityData();
-                    commodityData.CommodityID = resultDB[_index].COMMODITY_ID;
-                    commodityData.CurrentYearEnd = Convert.ToDecimal(resultDB[_index].CURRENT_YEAR_END);
-                    //commodityData.LastUpdate = Convert.ToDateTime(resultDB[_index].LASTUPDATE);
-                    commodityData.LongTerm = Convert.ToDecimal(resultDB[_index].LONG_TERM);
-                    commodityData.NextYearEnd = Convert.ToDecimal(resultDB[_index].NEXT_YEAR_END);
-                    if (commodityData.CommodityID == calculatedData.CommodityID)
+                    foreach (FXCommodityData item in calculatedViewResult)
                     {
-                        commodityData.YTD = calculatedData.YTD;
-                        commodityData.Year1 = calculatedData.Year1;
-                        commodityData.Year3 = calculatedData.Year3;
+                        if (resultDB[_index].COMMODITY_ID == item.CommodityId)
+                        {
+                            FXCommodityData commodityData = new FXCommodityData();
+
+                            commodityData.CommodityId = resultDB[_index].COMMODITY_ID;
+
+                            //Columns coming from Commodity table
+                            commodityData.CurrentYearEnd = Convert.ToDecimal(resultDB[_index].CURRENT_YEAR_END);
+                            //commodityData.LastUpdate = Convert.ToDateTime(resultDB[_index].LASTUPDATE);
+                            commodityData.LongTerm = Convert.ToDecimal(resultDB[_index].LONG_TERM);
+                            commodityData.NextYearEnd = Convert.ToDecimal(resultDB[_index].NEXT_YEAR_END);
+
+                            //Columns coming from Pricing View
+                            commodityData.YTD = item.YTD;
+                            commodityData.Year1 = item.Year1;
+                            commodityData.Year3 = item.Year3;
+
+                            result.Add(commodityData);
+                        }
                     }
-                    result.Add(commodityData);
+                   
                 }
 
                 return result;
@@ -318,9 +367,10 @@ namespace GreenField.Web.Services
             for (int i = 0; i < commodityData.Count; i++)
             {
                 FXCommodityData data = new FXCommodityData();
-                data.CommodityID = commodityData[i].COMMODITY_ID;
+                data.CommodityId = commodityData[i].COMMODITY_ID;
                 result.Add(data);
             }
+            result.Add(new FXCommodityData { CommodityId = GreenfieldConstants.COMMODITY_ALL});
             return result;
         }
     }
