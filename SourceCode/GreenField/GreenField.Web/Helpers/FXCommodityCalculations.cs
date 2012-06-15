d2e6c9e8e2a5361550567697b6d98b19ca396db9
seema <none@none>
2012-06-15 12:05:55 +0530

    commodity changes

--- SourceCode/GreenField/GreenField.Web/Helpers/FXCommodityCalculations.cs
+++ SourceCode/GreenField/GreenField.Web/Helpers/FXCommodityCalculations.cs
@@ -10,9 +10,13 @@ namespace GreenField.Web.Helpers
     public class FXCommodityCalculations
     {
         public static FXCommodityData CalculateCommodityData(List<FXCommodityData> commodityData)
-        {            
+        {
+            decimal? GPToday = null;
+            decimal? GPLastYearEnd = null;
+            decimal? GP12MonthsAgo = null;
+            decimal? GP36MonthsAgo = null;
             DateTime CurrentDate = DateTime.Now.Date;
-            DateTime Date1DayBack = Convert.ToDateTime(CurrentDate.Month + "/" + (CurrentDate.Day - 1) + "/" + CurrentDate.Year);           
+            DateTime Date1DayBack = Convert.ToDateTime(CurrentDate.Month + "/" + (CurrentDate.Day - 1) + "/" + CurrentDate.Year);
             DateTime DateLastYearEnd = Convert.ToDateTime("12/31/" + (Date1DayBack.Year - 1));
             DateTime Date12MonthsAgo = Convert.ToDateTime(Date1DayBack.Month + "/" + Date1DayBack.Day + "/" + (Date1DayBack.Year - 1));//CurrentDate.AddYears(-1);
             DateTime Date36MonthsAgo = Convert.ToDateTime(Date1DayBack.Month + "/" + Date1DayBack.Day + "/" + (Date1DayBack.Year - 3)); //CurrentDate.AddYears(-3);
@@ -21,11 +25,6 @@ namespace GreenField.Web.Helpers
             
             foreach (FXCommodityData item in commodityData)
             {
-                decimal? GPToday = null;
-                decimal? GPLastYearEnd = null;
-                decimal? GP12MonthsAgo = null;
-                decimal? GP36MonthsAgo = null;
-
                 if (item.FromDate == Date1DayBack)
                     GPToday = item.DailyClosingPrice;
 
@@ -37,19 +36,20 @@ namespace GreenField.Web.Helpers
 
                 if (item.FromDate == Date36MonthsAgo)
                     GP36MonthsAgo = item.DailyClosingPrice;
-           
-                if (GPToday != null && GPLastYearEnd != null)
-                    result.YTD = (GPToday - GPLastYearEnd) / GPLastYearEnd * 100;
 
-                if (GPToday != null && GP12MonthsAgo != null)
-                    result.Year1 = (GPToday - GP12MonthsAgo) / GP12MonthsAgo * 100;
-
-                if (GPToday != null && GP36MonthsAgo != null)
-                    result.Year3 = (GPToday - GP36MonthsAgo) / GP36MonthsAgo * 100;
-                
                 result.CommodityId = item.CommodityId;
-                result.InstrumentId = item.InstrumentId;
+                //result.InstrumentId = item.InstrumentId;
             }
+
+            if (GPToday != null && GPLastYearEnd != null)
+                result.YTD = (GPToday - GPLastYearEnd) / GPLastYearEnd * 100;
+
+            if (GPToday != null && GP12MonthsAgo != null)
+                result.Year1 = (GPToday - GP12MonthsAgo) / GP12MonthsAgo * 100;
+
+            if (GPToday != null && GP36MonthsAgo != null)
+                result.Year3 = (GPToday - GP36MonthsAgo) / GP36MonthsAgo * 100;
+
             return result;
         }
     }
