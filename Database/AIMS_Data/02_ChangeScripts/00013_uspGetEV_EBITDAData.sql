a37916fcb215d3cb71e7ead43079819efb0375e1
seema <none@none>
2012-08-14 18:36:18 +0530

    changes.

--- Database/AIMS_Data/02_ChangeScripts/00013_uspGetEV_EBITDAData.sql
+++ Database/AIMS_Data/02_ChangeScripts/00013_uspGetEV_EBITDAData.sql
@@ -27,53 +27,48 @@ CREATE PROCEDURE [dbo].[Get_EV_EBITDA](@securityId varchar(20),@issuerId varchar
 AS
 BEGIN
 	SET NOCOUNT ON;
-	declare @ISSUER_ID varchar(20)
-declare @SECURITY_ID varchar(20)
-set @ISSUER_ID = '25199390'
-set @SECURITY_ID = '157240' 
-
-	-- Get the ISSUER_ID
-		select @ISSUER_ID = convert(varchar(20),ISSUER_ID) from GF_SECURITY_BASEVIEW
-		where SECURITY_ID = convert(varchar(20),@SECURITY_ID)
-		print 'ISSUER_ID = '+ @ISSUER_ID
-		
+	
 	------Prices/Shares/FX Rates----------
 	Select a.SECURITY_ID, a.PRICE_DATE
 		, (a.price/case when a.ADR_CONV = 0.0 then 1.0 else a.ADR_CONV end)/ fx.FX_RATE as USDPrice
 		, fx.CURRENCY
 		, b.SHARES_OUTSTANDING 
-	into #SupportData
-	from PRICES a
-			inner join GF_SECURITY_BASEVIEW sb			on sb.SECURITY_ID = a.SECURITY_ID
-			inner join FX_RATES fx						on fx.CURRENCY = sb.TRADING_CURRENCY and fx.FX_DATE = a.PRICE_DATE
-			inner join ISSUER_SHARES b					on b.SHARES_DATE = fx.FX_DATE and b.ISSUER_ID = sb.ISSUER_ID
-	where sb.SECURITY_ID = @SECURITY_ID
+into #SupportData
+from PRICES a
+		inner join GF_SECURITY_BASEVIEW sb			on sb.SECURITY_ID = a.SECURITY_ID
+		inner join FX_RATES fx						on fx.CURRENCY = sb.TRADING_CURRENCY and fx.FX_DATE = a.PRICE_DATE
+		inner join ISSUER_SHARES b					on b.SHARES_DATE = fx.FX_DATE and b.ISSUER_ID = sb.ISSUER_ID
+where sb.SECURITY_ID = @securityId
 	
-	-----EXTRACT QUARTER VALUES FROM PRIMARY ANALYST SOURCE FIRST---	
-	Select * 
-	into #PrimaryFinancials
-	from PERIOD_FINANCIALS 
-	where (@ISSUER_ID is null or ISSUER_ID = '8233223')
-			and PERIOD_END_DATE > dateadd (year, -10, getdate())
-			and PERIOD_END_DATE < dateadd (year, 6, getdate())
-			-- and DATA_SOURCE = 'PRIMARY' -- currently no data for Primary analyst
-			and FISCAL_TYPE = 'FISCAL'
-			and Currency = 'USD'
-			and PERIOD_TYPE in ('Q1','Q2','Q3','Q4')
-	order by ISSUER_ID, DATA_ID, PERIOD_YEAR
+	
+	-----EXTRACT QUARTER VALUES FROM PRIMARY ANALYST SOURCE FIRST---
+	
+Select * 
+into #PrimaryFinancials
+from PERIOD_FINANCIALS 
+where (@issuerId is null or ISSUER_ID = @issuerId)
+		and PERIOD_END_DATE > dateadd (year, -10, getdate())
+		and PERIOD_END_DATE < dateadd (year, 6, getdate())
+		-- and DATA_SOURCE = 'PRIMARY' -- currently no data for Primary analyst
+		and FISCAL_TYPE = 'FISCAL'
+		and Currency = 'USD'
+		and PERIOD_TYPE in ('Q1','Q2','Q3','Q4')
+order by ISSUER_ID, DATA_ID, PERIOD_YEAR
 
-	Select * 
-	into #ConsensusFinancials
-	from CURRENT_CONSENSUS_ESTIMATES cce
-	where (@ISSUER_ID is null or ISSUER_ID = @ISSUER_ID)
-			and PERIOD_END_DATE > (select max(PERIOD_END_DATE) as PERIOD_END_DATE from #PrimaryFinancials)
-			and DATA_SOURCE = 'REUTERS' 
-			and FISCAL_TYPE = 'FISCAL'
-			and Currency = 'USD'
-			and PERIOD_TYPE in ('Q1','Q2','Q3','Q4')
-	order by ISSUER_ID, estimate_id, PERIOD_YEAR    
-	 
+-- *next pull quarter estimated values for periods after the oldest PeriodEndDate in the select above
 
+Select * 
+into #ConsensusFinancials
+from CURRENT_CONSENSUS_ESTIMATES cce
+where (@issuerId is null or ISSUER_ID = @issuerId)
+		and PERIOD_END_DATE > (select max(PERIOD_END_DATE) as PERIOD_END_DATE from #PrimaryFinancials)
+		and DATA_SOURCE = 'REUTERS' 
+		and FISCAL_TYPE = 'FISCAL'
+		and Currency = 'USD'
+		and PERIOD_TYPE in ('Q1','Q2','Q3','Q4')
+order by ISSUER_ID, estimate_id, PERIOD_YEAR
+
+   --Joining tables depending upon the chart title
  IF(@chartTitle = 'EV/EBITDA')
 			BEGIN
 				
@@ -90,8 +85,8 @@ set @SECURITY_ID = '157240'
 									,  DATA_ID
 							from #PrimaryFinancials
 							where DATA_ID in (90,51,92,29,24)
-							)ev_ebitda						
-						
+							)ev_ebitda
+							
 							
 					--*From above extract, group by PeriodType and PeriodYear calculating two values (Net Debt and EBITDA) for each Period by aggregating DataID’s per the following into datset EVEBITDA
 					SELECT  PERIOD_TYPE + ' ' + CAST(PERIOD_YEAR AS VARCHAR(4)) as PeriodLabel
@@ -122,6 +117,8 @@ set @SECURITY_ID = '157240'
 							  ) AS pvt 
                   
                   
+                  --select periodlabel,[90]+[92]-[51] as NetDebt from #PIVOT_EVBTIDA_NetDebt
+                  
                   SELECT  PERIOD_TYPE + ' ' + CAST(PERIOD_YEAR AS VARCHAR(4)) as PeriodLabel
 						  ,PERIOD_END_DATE
 						  ,[29] 
@@ -148,7 +145,7 @@ set @SECURITY_ID = '157240'
 										FOR Data_id   IN ([29],[24])
 									  ) AS pvt 
                   
-                                 
+                  
                   --CALCULATING NetDebt and EBITDA
                   Select e.PeriodLabel
 						  ,e.PERIOD_END_DATE
@@ -160,33 +157,36 @@ set @SECURITY_ID = '157240'
                   on
                   e.PeriodLabel = n.PeriodLabel
                   
-						
-				--Join EVEBITDA dataset  to SupportData on PeriodEndDates
-				Select a.*, b.USDPrice, b.SHARES_OUTSTANDING 
-				into #EVEBITDASupport
-				from #EVEBITDA a					
-				left join #SupportData b 
-				on a.PERIOD_END_DATE = b.PRICE_DATE
-				order by  a.PERIOD_YEAR, a.PERIOD_TYPE
+                  
+					--Join EVEBITDA dataset  to SupportData on PeriodEndDates
+					Select a.*, b.USDPrice, b.SHARES_OUTSTANDING 
+					into #EVEBITDASupport
+					from #EVEBITDA a					
+					left join #SupportData b 
+					on a.PERIOD_END_DATE = b.PRICE_DATE
+					order by  PeriodLabel
 
-				
-				--SELECTING CALCULATED DATA
-				
-				SELECT * FROM #EVEBITDASupport
-				
-				--Dropping above tables
-				DROP TABLE #RevenueValuesEV_EBITDA
-				DROP TABLE #PIVOT_EVBTIDA_NetDebt	
-				DROP TABLE #PIVOT_EVBTIDA_EBITDA	
-				DROP TABLE #EVEBITDA
+					
+					--SELECTING CALCULATED DATA
+					
+					SELECT * FROM #EVEBITDASupport
+					
+					--Dropping above tables
+					DROP TABLE #RevenueValuesEV_EBITDA
+					DROP TABLE #PIVOT_EVBTIDA_NetDebt	
+					DROP TABLE #PIVOT_EVBTIDA_EBITDA	
+					DROP TABLE #EVEBITDA
 			END	
+			
+	
+ 
+	
 	--Dropping temporary tables
 	drop table #SupportData
 	drop table #PrimaryFinancials
 	drop table #ConsensusFinancials	
 
 END
-
 GO
 --indicate thet current script is executed
 declare @CurrentScriptVersion as nvarchar(100) = '00013'
