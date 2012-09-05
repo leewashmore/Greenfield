0ff576e24441fc98a2385ff5d07678bf2d643124
sakshi <none@none>
2012-09-05 14:28:09 +0530

    Updated View and view model for custom screening tool

---
+++ SourceCode/GreenField/GreenField.DataContracts/DataContracts/CustomSelectionData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace GreenField.DataContracts.DataContracts
+{
+    [DataContract]
+    public class CustomSelectionData
+    {
+        [DataMember]
+        public string Country { get; set; }
+
+        [DataMember]
+        public string Region { get; set; }
+
+        [DataMember]
+        public string Sector { get; set; }
+
+        [DataMember]
+        public string Industry { get; set; }
+    }
+}
