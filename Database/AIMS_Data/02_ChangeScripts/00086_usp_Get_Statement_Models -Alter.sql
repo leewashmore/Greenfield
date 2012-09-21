2842b7c9db73d13eb9a4eb0d6660c2bee772c8a4
IndiaUser1 <none@none>
2012-09-21 21:37:45 +0530

    updated

--- Database/AIMS_Data/02_ChangeScripts/00086_usp_Get_Statement_Models -Alter.sql
+++ Database/AIMS_Data/02_ChangeScripts/00086_usp_Get_Statement_Models -Alter.sql
@@ -2,8 +2,8 @@ set noexec off
 
 --declare  current and required version
 --also do it an the end of the script
-declare @RequiredDBVersion as nvarchar(100) = '00084'
-declare @CurrentScriptVersion as nvarchar(100) = '00085'
+declare @RequiredDBVersion as nvarchar(100) = '00085'
+declare @CurrentScriptVersion as nvarchar(100) = '00086'
 
 --if current version already in DB, just skip
 if exists(select 1 from ChangeScripts  where ScriptVersion = @CurrentScriptVersion)
@@ -137,5 +137,5 @@ DROP TABLE #CONSENSUS_ACTUAL_DATA
 DROP TABLE #EXTERNAL_ACTUAL_DATA
 GO
 --indicate thet current script is executed
-declare @CurrentScriptVersion as nvarchar(100) = '00085'
+declare @CurrentScriptVersion as nvarchar(100) = '00086'
 insert into ChangeScripts (ScriptVersion, DateExecuted ) values (@CurrentScriptVersion, GETDATE())
