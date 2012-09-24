7da05c06c848b6401900bc38303131368bb50baf
Lane Summers <lanes@emiemm.com>
2012-09-24 14:35:10 -0400

    Bug Fix: Jira TESTDEVPROJ-397 http://10.3.48.18:8080/browse/TESTDEVPROJ-397 This is just the first part of the fix.  The bug concerts 2 gadgets and this fixes one of them.

---
+++ SourceCode/GreenField/GreenField.Gadgets/Helpers/Converters/BasisPointsConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+
+
+namespace GreenField.Gadgets.Helpers
+{
+    public class BasisPointsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Decimal basisPoints = (Decimal)value * 10000;
+            return basisPoints.ToString();
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
