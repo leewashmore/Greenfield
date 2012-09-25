535f2e2cba6cdf53bc160a8e3e687753e06cd326
Lane Summers <lanes@emiemm.com>
2012-09-25 09:31:52 -0400

    Bug Fix: TESTDEVPROJ-397 http://10.3.48.18:8080/browse/TESTDEVPROJ-397 This fixes the Contributors detractopr gadget to display in basis points.

--- SourceCode/GreenField/GreenField.Gadgets/Helpers/Converters/BasisPointsConverter.cs
+++ SourceCode/GreenField/GreenField.Gadgets/Helpers/Converters/BasisPointsConverter.cs
@@ -9,7 +9,7 @@ namespace GreenField.Gadgets.Helpers
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             Decimal basisPoints = (Decimal)value * 10000;
-            return basisPoints.ToString();
+            return basisPoints.ToString("n0");  //return basis points with no decimal places
         }
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
