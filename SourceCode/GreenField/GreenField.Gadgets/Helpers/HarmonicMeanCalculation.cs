0b973c9beab055bf8278dd8ae8400896bc1d0030
sakshi <none@none>
2012-09-26 19:25:18 +0530

    CST Harmonic Mean Added

--- SourceCode/GreenField/GreenField.Gadgets/Helpers/HarmonicMeanCalculation.cs
+++ SourceCode/GreenField/GreenField.Gadgets/Helpers/HarmonicMeanCalculation.cs
@@ -12,6 +12,8 @@ using Telerik.Windows.Data;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using GreenField.Gadgets.Models;
 
 namespace GreenField.Gadgets.Helpers
 {
@@ -36,26 +38,61 @@ namespace GreenField.Gadgets.Helpers
 
     public class HarmonicMean
     {
-        public static double HarmonicMeanCalculationMethod<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        public static Decimal HarmonicMeanCalculationMethod<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
         {
+                       
+            Decimal totalweight = 0;
+            Decimal totalharmonic = 0;
+           
             List<MyDataRow> sourceData = source.Cast<MyDataRow>().ToList();
             List<string> marketCapList = new List<string>();
 
             for (int i = 0; i < sourceData.Count; i++)
-            {
+            {                
                 string marketCap = sourceData[0]["Market Capitalization"].ToString();
                 marketCapList.Add(marketCap);
             }
 
             List<String> numericvalues = new List<string>();
-            IEnumerable<string> values = from i in source 
+            IEnumerable<string> values = from i in source
                                          select Convert.ToString(selector(i)).ToLower();
             foreach (String strValue in values)
             {
                 numericvalues.Add(strValue);
             }
 
-            return 100;
+            for (int i = 4; i < numericvalues.Count(); i++) 
+            {
+                if (marketCapList[i] != null && numericvalues[i] != null && marketCapList[i] != String.Empty && numericvalues[i] != String.Empty)
+                {
+                    totalweight = totalweight + Convert.ToDecimal(marketCapList[i]);
+                }
+            }       
+
+            for (int i = 4; i < numericvalues.Count(); i++)
+            {
+                if (marketCapList[i] != null && numericvalues[i] != null)
+                {
+                    if (totalweight != 0 && numericvalues[i] != String.Empty && marketCapList[i] != String.Empty)
+                    {                       
+                        
+                        Decimal weight = Convert.ToDecimal(marketCapList[i])/ totalweight;
+                        Decimal amount = 1/Convert.ToDecimal(numericvalues[i]);
+                        Decimal inverseAmount = weight * amount;
+                        totalharmonic = totalharmonic + inverseAmount;
+                    }
+                }
+            }
+
+            if (totalharmonic != 0)
+            {
+                return (1 / totalharmonic);
+            }
+            else
+            {
+                return 0;
+            }         
+
         }
     }
 }
