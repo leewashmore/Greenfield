a00a63ae3aa724aed21606aea19c04602084a901
gtiwari <none@none>
2012-09-26 17:55:49 +0530

    CST harmonic mean calculation

--- SourceCode/GreenField/GreenField.Gadgets/Helpers/HarmonicMeanCalculation.cs
+++ SourceCode/GreenField/GreenField.Gadgets/Helpers/HarmonicMeanCalculation.cs
@@ -38,8 +38,22 @@ namespace GreenField.Gadgets.Helpers
     {
         public static double HarmonicMeanCalculationMethod<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> selector)
         {
+            List<MyDataRow> sourceData = source.Cast<MyDataRow>().ToList();
+            List<string> marketCapList = new List<string>();
+
+            for (int i = 0; i < sourceData.Count; i++)
+            {
+                string marketCap = sourceData[0]["Market Capitalization"].ToString();
+                marketCapList.Add(marketCap);
+            }
+
+            List<String> numericvalues = new List<string>();
             IEnumerable<string> values = from i in source 
                                          select Convert.ToString(selector(i)).ToLower();
+            foreach (String strValue in values)
+            {
+                numericvalues.Add(strValue);
+            }
 
             return 100;
         }
