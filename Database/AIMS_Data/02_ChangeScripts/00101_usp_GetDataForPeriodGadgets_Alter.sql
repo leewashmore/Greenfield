a645ef79b8c51d5eca183c684f7e11f074b4299c
gtiwari <none@none>
2012-09-29 16:28:38 +0530

    corrected the required version

--- Database/AIMS_Data/02_ChangeScripts/00101_usp_GetDataForPeriodGadgets_Alter.sql
+++ Database/AIMS_Data/02_ChangeScripts/00101_usp_GetDataForPeriodGadgets_Alter.sql
@@ -2,7 +2,7 @@ set noexec off
 
 --declare  current and required version
 --also do it an the end of the script
-declare @RequiredDBVersion as nvarchar(100) = '000100'
+declare @RequiredDBVersion as nvarchar(100) = '00100'
 declare @CurrentScriptVersion as nvarchar(100) = '00101'
 
 --if current version already in DB, just skip
