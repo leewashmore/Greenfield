5d5abef43ab2d900635279d173102bc3d69ede2b
vitaliydev@GHDEV.emiemm.com <vitaliydev@GHDEV.emiemm.com>
2012-11-12 09:37:47 -0500

    a sketch of the Targeting WCF service has been added
    --HG--
    branch : targeting

---
+++ SourceCode/GreenField/GreenField.Web/Services/TargetingOperations.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Activation;
+using System.Configuration;
+using System.Net.Mail;
+using System.Resources;
+using GreenField.Web.Helpers.Service_Faults;
+using GreenField.Web.Helpers;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+namespace GreenField.Web.Services
+{
+    
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class TargetingOperations : TopDown.FacingClient.Facade
+    {
+    }
+}
