c34beb87382cd6563b9321043937d6939e731eda
AlekseyB@WASWK060.ASHMORE.LOCAL <AlekseyB@WASWK060.ASHMORE.LOCAL>
2012-12-10 14:05:26 -0500

    issuesshares module

---
+++ SourceCode/GreenField/IssuerShares/GreenField.IssuerShares.App/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using Microsoft.Practices.Prism.Regions;
+
+namespace GreenField.IssuerShares.App
+{
+    [Export]
+    public class ShellViewModel
+    {
+        private IRegionManager regionManager;
+
+        [ImportingConstructor]
+        public ShellViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+    }
+}
