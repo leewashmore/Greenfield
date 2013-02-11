f8d2b0b609e324f291e83a49a1709aec38299d7f
AlekseyB@WASWK060.ASHMORE.LOCAL <AlekseyB@WASWK060.ASHMORE.LOCAL>
2013-02-11 09:15:22 -0500

    Add utility to update SQL scripts during deployment

---
+++ SourceCode/SqlReplacer/SqlReplacer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NDesk.Options;
+using System.Diagnostics;
+using System.Xml;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SqlReplacer
+{
+    class Program
+    {
+        private class Rule
+        {
+            [DebuggerStepThrough]
+            public Rule(string from, string to)
+            {
+                this.From = from;
+                this.To = to;
+            }
+        
+            public String From { get; private set; }
+            public String To { get; private set; }
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+            String ruleFilePath = null;
+            String find = null;
+            String replaceTo = null;
+            String file = null;
+
+            OptionSet options = new OptionSet();
+            options.Add("r=", "path to xml file containing set of rules for replacement", value => ruleFilePath = value);
+            options.Add("f=", "what needs to be replaced", value => find = value);
+            options.Add("t=", "replacement value", value => replaceTo = value);
+            options.Add("u=", "file for update", value => file = value);
+            options.Parse(args);
+
+            List<Rule> rules = new List<Rule>();
+            if (file == null)
+            {
+               rules.Add(new Rule(find, replaceTo));
+            }
+            else
+            {
+                var xml = new XmlDocument();
+                xml.Load(ruleFilePath);
+                var nodes = xml.SelectNodes("//rule");
+                foreach (XmlNode node in nodes)
+                {
+                    rules.Add(new Rule(node.Attributes["from"].Value, node.Attributes["to"].Value));
+                }
+            }
+
+            string fileContent;
+            using (var sr = new StreamReader(file))
+            {
+                fileContent = sr.ReadToEnd();
+            }
+            foreach (var rule in rules)
+            {
+                fileContent = Regex.Replace(fileContent, rule.From, rule.To, RegexOptions.IgnoreCase);
+            }
+            using (var sw = new StreamWriter(file))
+            {
+                sw.Write(fileContent);
+            }
+        }
+    }
+}
