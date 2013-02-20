f02b0aee140efb96525ff00be5a924ca241d3a2d
Krish <none@none>
2013-02-20 11:43:05 -0500

    (New) added refresjcache support

--- SourceCode/GreenField/GreenField.Web/Helpers/DefaultCacheProvider.cs
+++ SourceCode/GreenField/GreenField.Web/Helpers/DefaultCacheProvider.cs
@@ -6,17 +6,34 @@ namespace GreenField.Web.Helpers
 {
     public sealed class CacheKeyNames
     {
+        /*
+        public const string SECURITY = "SECURITY";
+        public const string BENCHMARK = "BENCHMARK";
+        public const string INDEX = "INDEX";
+        public const string COMMODITY = "COMMODITY";
+        public const string CURRENCY = "CURRENCY";
+         */
+
+        public const string EntitySelectionDataCache = "EntitySelectionDataCache";
         public const string FilterSelectionDataCache = "FilterSelectionDataCache";
         public const string PortfolioSelectionDataCache = "PortfolioSelectionDataCache";
+
         public const string AvailableDatesInPortfoliosCache = "AvailableDatesInPortfoliosCache";
         public const string CountrySelectionDataCache = "CountrySelectionDataCache";
         public const string RegionSelectionDataCache = "RegionSelectionDataCache";
+        
         public const string FXCommodityDataCache = "FXCommodityDataCache";
         public const string MarketSnapshotSelectionDataCache = "MarketSnapshotSelectionDataCache";
         public const string LastDayOfMonthsCache = "LastDayOfMonthsCache";
-        public const string EntitySelectionDataCache = "EntitySelectionDataCache";
     }
-    
+
+    public class CacheExpiration
+    {
+        public string CacheKeyName { get; set; }
+        public string ShortName { get; set; }
+        public DateTimeOffset? AbsoluteExpiration { get; set; }
+    }
+
     public interface ICacheProvider
     {
         object Get(string key);
@@ -41,39 +58,80 @@ namespace GreenField.Web.Helpers
         {
             CacheItemPolicy policy = new CacheItemPolicy();
             policy.AbsoluteExpiration = DateTime.Now.AddMinutes(cacheTime);
-            //can also use: policy.RemovedCallback = new CacheEntryRemovedCallback (ItemRemoved);
+            //Hint: can also use policy.RemovedCallback = new CacheEntryRemovedCallback (ItemRemoved);
 
             cache.Add(new CacheItem(key, data), policy);
-            cache.Add(new CacheItem(key + "Policy", data), null);
+            cache.Add(new CacheItem(key + "Policy", policy), null);
         }
 
-        /*
-        public DateTime GetExpiration(string key)
+        public CacheExpiration GetExpiration(string key)
         {
-            return  ((CacheItemPolicy)cache[key + "Policy"]).AbsoluteExpiration;
-        }*/
+            if ((CacheItemPolicy)cache[key + "Policy"] != null)
+            {
+                CacheExpiration cacheExpiration = new CacheExpiration();
+                
+                cacheExpiration.CacheKeyName = key;
+                cacheExpiration.AbsoluteExpiration = ((CacheItemPolicy)cache[key + "Policy"]).AbsoluteExpiration;
 
-        public List<DateTime> GetExpiration()
-        {
+                switch (key)
+                {
+                    case CacheKeyNames.EntitySelectionDataCache:
+                        cacheExpiration.ShortName = "Entity";
+                        break;
+                    case CacheKeyNames.FilterSelectionDataCache:
+                        cacheExpiration.ShortName = "Filter";
+                        break;
+                    case CacheKeyNames.PortfolioSelectionDataCache:
+                        cacheExpiration.ShortName = "Portfolio";
+                        break;
+
+                    case CacheKeyNames.AvailableDatesInPortfoliosCache:
+                        cacheExpiration.ShortName = "Dates";
+                        break;
+                    case CacheKeyNames.CountrySelectionDataCache:
+                        cacheExpiration.ShortName = "Country";
+                        break;
+                    case CacheKeyNames.RegionSelectionDataCache:
+                        cacheExpiration.ShortName = "Region";
+                        break;
+
+                    case CacheKeyNames.FXCommodityDataCache:
+                        cacheExpiration.ShortName = "FXCommodity";
+                        break;
+                    case CacheKeyNames.MarketSnapshotSelectionDataCache:
+                        cacheExpiration.ShortName = "MarketSnapshot";
+                        break;
+                    case CacheKeyNames.LastDayOfMonthsCache:
+                        cacheExpiration.ShortName = "DayOfMonths";
+                        break;
+                }
+
+                return cacheExpiration;
+            }
+
             return null;
+        }
 
-            /*
-            List<DateTime> dates = new List<DateTime>;
+        public List<CacheExpiration> GetAllExpirations()
+        {
+            List<CacheExpiration> dates = new List<CacheExpiration>();
 
-            dates.add(GetExpiration(FilterSelectionDataCache));
-            dates.add(GetExpiration(PortfolioSelectionDataCache));
-            dates.add(GetExpiration(AvailableDatesInPortfoliosCache));
-            dates.add(GetExpiration(CountrySelectionDataCache));
-            dates.add(GetExpiration(RegionSelectionDataCache));
-            dates.add(GetExpiration(FXCommodityDataCache));
-            dates.add(GetExpiration(MarketSnapshotSelectionDataCache));
-            dates.add(GetExpiration(LastDayOfMonthsCache));
-            dates.add(GetExpiration(EntitySelectionDataCache));
+            dates.Add(GetExpiration(CacheKeyNames.EntitySelectionDataCache));
+            dates.Add(GetExpiration(CacheKeyNames.FilterSelectionDataCache));
+            dates.Add(GetExpiration(CacheKeyNames.PortfolioSelectionDataCache));
+
+            dates.Add(GetExpiration(CacheKeyNames.AvailableDatesInPortfoliosCache));
+            dates.Add(GetExpiration(CacheKeyNames.CountrySelectionDataCache));
+            dates.Add(GetExpiration(CacheKeyNames.RegionSelectionDataCache));
+
+            dates.Add(GetExpiration(CacheKeyNames.FXCommodityDataCache));
+            dates.Add(GetExpiration(CacheKeyNames.MarketSnapshotSelectionDataCache));
+            dates.Add(GetExpiration(CacheKeyNames.LastDayOfMonthsCache));
 
             return dates;
-             */
         }
 
+        // Not used
         public bool IsSet(string key)
         {
             return (cache[key] != null);
