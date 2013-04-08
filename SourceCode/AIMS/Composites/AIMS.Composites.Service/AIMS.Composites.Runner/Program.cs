3c9ecef6d782cf316d93946fa82b223d03b11353
Krish <none@none>
2013-04-08 16:24:18 -0400

    (New): Created separate VS 2012 WCF service and console application projects to retrieve composites.

---
+++ SourceCode/AIMS/Composites/AIMS.Composites.Service/AIMS.Composites.Runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS.Composites.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SourceCode/AIMS/Composites/AIMS.Composites.Service/AIMS.Composites.Service/CompositesOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AIMS.Composites.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CompositesOperations" in both code and config file together.
+    public class CompositesOperations : ICompositesOperations
+    {
+        public void DoWork()
+        {
+            //compo
+        }
+    }
+}
