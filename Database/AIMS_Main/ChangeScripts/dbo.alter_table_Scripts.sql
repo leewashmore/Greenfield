12f94da4ac998936a84dc4bd4f8f2db27d44dbb7
naziralia <Akhtar.Nazirali@Ashmoregroup.com>
2013-04-18 13:14:10 -0400

    (mod) alter table script

--- Database/AIMS_Main/ChangeScripts/dbo.alter_table_Scripts.sql
+++ Database/AIMS_Main/ChangeScripts/dbo.alter_table_Scripts.sql
@@ -1,11 +1,17 @@
 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
 alter table dbo.data_master add MinValue decimal(18, 2);
+GO
 alter table dbo.data_master add MaxValue decimal(18, 2);	
-
+GO
 alter table dbo.gf_security_baseview add issuer_proxy varchar(20);
+GO
 alter table dbo.gf_security_baseview add UPDATE_BB_STATUS varchar(20);
-
+GO
 update dbo.data_master set MinValue =0,MaxValue =0.9 where data_id =133;
 update dbo.data_master set MinValue =0,MaxValue =10 where data_id =164;
 update dbo.data_master set MinValue =0,MaxValue =60 where data_id =166;
@@ -14,4 +20,5 @@ update dbo.data_master set MinValue =0,MaxValue =60 where data_id =187;
 update dbo.data_master set MinValue =0,MaxValue =60 where data_id =193;
 update dbo.data_master set MinValue =0,MaxValue =0.3 where data_id =192;
 update dbo.data_master set MinValue =0 where data_id =185;
+GO
 
