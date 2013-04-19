90d5b4c261cb94f65265252eddb12d67ee259650
naziralia <Akhtar.Nazirali@Ashmoregroup.com>
2013-04-19 09:27:40 -0400

    (Mod) Holdings Gadget changes

--- SourceCode/GreenField/GreenField.Web/Services/BenchmarkHoldingsOperations.svc.cs
+++ SourceCode/GreenField/GreenField.Web/Services/BenchmarkHoldingsOperations.svc.cs
@@ -1176,7 +1176,7 @@ namespace GreenField.Web.Services
                 List<GF_COMPOSITE_LTHOLDINGS> compositeHoldingsData = null;
                 ExternalResearchEntities externalEntity = new GreenField.DAL.ExternalResearchEntities();
                 Boolean isFiltered = false;
-                Boolean isComposite = externalEntity.COMPOSITE_MASTER.Where(x => x.COMPOSITE_ID.ToUpper() == objPortfolioIdentifier.PortfolioId.ToUpper()).Count() == 0 ? false : true;
+                Boolean isComposite = objPortfolioIdentifier.IsComposite;
 
                 dimensionPortfolioLTHoldingsData = null;
                 if (lookThruEnabled)
