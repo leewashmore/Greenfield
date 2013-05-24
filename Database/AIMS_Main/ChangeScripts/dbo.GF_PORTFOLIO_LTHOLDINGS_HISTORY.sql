0bd90d877c33393d61fe3646bfa527b5f3635354
naziralia <Akhtar.Nazirali@Ashmoregroup.com>
2013-05-24 10:19:05 -0400

    (Mod) Marketing Calc

--- Database/AIMS_Main/ChangeScripts/dbo.GF_PORTFOLIO_LTHOLDINGS_HISTORY.sql
+++ Database/AIMS_Main/ChangeScripts/dbo.GF_PORTFOLIO_LTHOLDINGS_HISTORY.sql
@@ -1,9 +1,5 @@
 
 
-/****** Object:  Table [dbo].[GF_PORTFOLIO_LTHOLDINGS_HISTORY]    Script Date: 05/01/2013 11:32:40 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[GF_PORTFOLIO_LTHOLDINGS_HISTORY]') AND type in (N'U'))
-DROP TABLE [dbo].[GF_PORTFOLIO_LTHOLDINGS_HISTORY]
-GO
 
 
 
