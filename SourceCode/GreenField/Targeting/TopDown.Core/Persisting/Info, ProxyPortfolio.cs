dc360392a4806a1870a3971d912b671df9b2140d
naziralia <Akhtar.Nazirali@Ashmoregroup.com>
2013-06-27 11:15:16 -0400

    (New) Targeting changes adding new class to persist proxy portfolio

---
+++ SourceCode/GreenField/Targeting/TopDown.Core/Persisting/Info, ProxyPortfolio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace TopDown.Core.Persisting
+{
+    public class ProxyPortfolioInfo
+    {
+        [DebuggerStepThrough]
+        public ProxyPortfolioInfo()
+        {
+        }
+        public String PortfolioId { get; set; }
+        public String ProxyPortfolioId { get; set; }
+        
+    }
+}
