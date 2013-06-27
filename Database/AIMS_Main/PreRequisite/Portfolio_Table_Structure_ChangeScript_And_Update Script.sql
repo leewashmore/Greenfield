a736796e0c58a39435e71e3c1edb62ea677dcb4b
naziralia <Akhtar.Nazirali@Ashmoregroup.com>
2013-06-27 11:31:18 -0400

    (Mod) Update statement for proxy portfolio

--- Database/AIMS_Main/PreRequisite/Portfolio_Table_Structure_ChangeScript_And_Update Script.sql
+++ Database/AIMS_Main/PreRequisite/Portfolio_Table_Structure_ChangeScript_And_Update Script.sql
@@ -1,6 +1,8 @@
 alter table dbo.portfolio add  proxy_portfolio varchar(20)
 Go
 
-update portfolio set proxy_portfolio = 'MUGCTM' where id = 'SICVESC'
-update portfolio set proxy_portfolio = 'GSCF' where id = 'USESC'
+update portfolio set proxy_portfolio = 'CURIANESC' where id = 'PASERSESC'
+update portfolio set proxy_portfolio = 'CURIANESC' where id = 'SICVESC'
+update portfolio set proxy_portfolio = 'CURIANESC' where id = 'USESC'
+
 Go
