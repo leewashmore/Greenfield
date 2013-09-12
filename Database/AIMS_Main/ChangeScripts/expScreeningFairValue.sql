9efcf90b757b01bf50a49427c7fb7ffc60b005a1
JustinM <none@none>
2013-09-12 15:21:51 -0400

    (MOD) Added Implied Buy and Sell price based on FV ranges to FV Screening XLS

--- Database/AIMS_Main/ChangeScripts/expScreeningFairValue.sql
+++ Database/AIMS_Main/ChangeScripts/expScreeningFairValue.sql
@@ -12,6 +12,13 @@ GO
 create procedure [dbo].[expScreeningFairValue]
 as
 
+--Figure out latest price date
+declare @PRICEDATE datetime
+
+select @PRICEDATE = MAX(p.PRICE_DATE)
+from dbo.PRICES p
+
+
 --extract full universe
 select gsb.ISSUER_ID, 
 	gsb.SECURITY_ID, 
@@ -25,7 +32,9 @@ select gsb.ISSUER_ID,
 	gsb.asec_sec_country_name as COUNTRY,
 	gsb.GICS_SECTOR_NAME as SECTOR,
 	gsb.GICS_INDUSTRY_NAME as INDUSTRY,
-	gsb.GICS_SUB_INDUSTRY_NAME as SUB_INDUSTRY
+	gsb.GICS_SUB_INDUSTRY_NAME as SUB_INDUSTRY,
+	gsb.TRADING_CURRENCY,
+	gsb.SHARES_PER_ADR
 into #S1	
 from dbo.GF_SECURITY_BASEVIEW gsb
 where gsb.SECURITY_TYPE not in ('BASKET EQ','FUND LP','FUND OEIC','PRV EQUITY')
@@ -74,7 +83,7 @@ and pf.DATA_SOURCE = 'PRIMARY'
 and pf.CURRENCY = 'USD'
 and pf.FISCAL_TYPE in ('CALENDAR','')
 and pf.PERIOD_TYPE in ('C')
-and pf.DATA_ID in (185,186,236,198,238,188,187,189)
+and pf.DATA_ID in (185,186,236,198,238,188,187,189,218)
 
 select s.security_id, 
 	s2.amount as PBV_BF24,
@@ -84,7 +93,8 @@ select s.security_id,
 	s6.amount as DY_BF24,
 	s7.amount as PNAV_BF24,
 	s.amount as MKT_CAP,
-	s1.amount as EV
+	s1.amount as EV,
+	s8.amount as SHARES
 into #SecLevel	
 from #S5 s
 left join #S5 s1 on s.security_id = s1.security_id and s1.data_id = 186
@@ -94,6 +104,7 @@ left join #S5 s4 on s.security_id = s4.security_id and s4.data_id = 189
 left join #S5 s5 on s.security_id = s5.security_id and s5.data_id = 198
 left join #S5 s6 on s.security_id = s6.security_id and s6.data_id = 236
 left join #S5 s7 on s.security_id = s7.security_id and s7.data_id = 238
+left join #S5 s8 on s.security_id = s8.security_id and s8.data_id = 218
 where s.data_id = 185 and s.period_type = 'C'
 
 
@@ -119,7 +130,7 @@ left join #S6 s3 on s.issuer_id = s3.issuer_id and s3.data_id = 300
 
 
 --Fair Value data
-select fv.SECURITY_ID, dm.DATA_DESC, fv.CURRENT_MEASURE_VALUE, fv.FV_BUY, fv.FV_SELL, fv.UPSIDE, fv.UPDATED
+select fv.SECURITY_ID, dm.DATA_DESC, fv.CURRENT_MEASURE_VALUE, fv.FV_BUY, fv.FV_SELL, fv.UPSIDE, fv.UPDATED, fv.FV_MEASURE
 into #FV
 from dbo.FAIR_VALUE fv
 join dbo.DATA_MASTER dm on fv.FV_MEASURE = dm.DATA_ID
@@ -130,7 +141,8 @@ select a.*,
 	B.MKT_VALUE,
 	B.BGA_WEIGHT,
 	C.ANALYST_UPDATE,
-	fv.updated AS FV_UPDATE, 
+	fv.updated AS FV_UPDATE,
+	fv.FV_MEASURE as FV_ID, 
 	fv.data_desc as FV_MEASURE,
 	fv.fv_buy as BUY,
 	fv.fv_sell as SELL,
@@ -143,21 +155,47 @@ select a.*,
 	e.PNAV_BF24,
 	e.DY_BF24,
 	E.MKT_CAP, 
+	E.SHARES,
 	E.EV, 
 	F.EQUITY_BF24, 
 	F.EARNINGS_BF24, 
 	case when PCE_BF24 = NULL then NULL else E.MKT_CAP/e.PCE_BF24 end as CASHEARN_BF24,
 	case when EV_EBITDA_BF24 = NULL then NULL else E.EV/e.EV_EBITDA_BF24 end as EBITDA_BF24,
 	case when PNAV_BF24 = NULL then NULL else E.MKT_CAP/e.PNAV_BF24 end as NAV_BF24,
- 	F.DIVIDENDS_BF24
---into #Screening	
+ 	F.DIVIDENDS_BF24,
+ 	p.PRICE,
+ 	fx.FX_RATE
+into #Screening	
 from #S1 a
 left join #S3 b on a.short_name = b.SHORT_NAME
 left join #S4 c on a.issuer_id = c.issuer_id
 left join #SecLevel e on a.security_id = E.security_id
 left join #IssLevel f on a.issuer_id = f.issuer_id
 left join #FV fv on a.security_id = fv.security_id
+left join dbo.PRICES p on a.security_id = p.SECURITY_ID and p.PRICE_DATE = @PRICEDATE
+left join dbo.FX_RATES fx on a.trading_currency = fx.CURRENCY and fx.FX_DATE = @PRICEDATE
 order by a.COUNTRY, a.ISSUE_NAME
+
+update #Screening
+set SHARES_PER_ADR = 1 where SHARES_PER_ADR = 0
+
+select *,
+case when FV_ID = 187 then ((BUY*EARNINGS_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 188 then ((BUY*EQUITY_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 189 then ((BUY*CASHEARN_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 238 then ((BUY*NAV_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 236 then (((DIVIDENDS_BF24/BUY)/ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE	
+	else 0 end as BUY_PRICE,
+case when FV_ID = 187 then ((SELL*EARNINGS_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 188 then ((SELL*EQUITY_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 189 then ((SELL*CASHEARN_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 238 then ((SELL*NAV_BF24*ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	when FV_ID = 236 then (((DIVIDENDS_BF24/SELL)/ISNULL(SHARES_PER_ADR,1))/SHARES)*FX_RATE
+	else 0 end as SELL_PRICE
+from #Screening s
+
+
+
 
 /*
 DECLARE @StartDT DATETIME = getdate()
@@ -183,6 +221,6 @@ drop table #S6
 drop table #SecLevel
 drop table #IssLevel
 drop table #FV
---drop table #Screening
+drop table #Screening
 
 go
